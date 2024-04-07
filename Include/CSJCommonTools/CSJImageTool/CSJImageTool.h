b9023bcd07f1fd3d1697c34d5274b71dbcd4cec7
zhongxh <zxhas@foxmail.com>
2024-04-07 17:04:07 +0800

    [improve] 添加CSJImageTool的依赖库

---
+++ Include/CSJCommonTools/CSJImageTool/CSJImageTool.h
@@ -0,0 +1,12 @@
+#ifndef __CSJIMAGETOOL_H__
+#define __CSJIMAGETOOL_H__
+
+#include "CSJCommonToolsDefine.h"
+
+class CSJCOMMONTOOLS_API CSJImageTool {
+public:
+    CSJImageTool() = default;
+    ~CSJImageTool() = default;
+};
+
+#endif
